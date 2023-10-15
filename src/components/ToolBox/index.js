0357e7fb2d7326a12e73578fc2126bc1acbca2f0
Hardik-Jain08 <hjain2734@gmail.com>
2023-10-16 04:21:03 +0530

    UI completed

---
+++ src/components/ToolBox/index.js
@@ -0,0 +1,32 @@
+import { MENU_ITEMS , COLORS} from '@/constants'
+import styles from './index.module.css'
+
+const Toolbox = () => {
+    const handleBrushSize = (e) => {
+
+    }
+
+    return (
+        <div className={styles.toolboxContainer}>
+            <div className={styles.toolItem}>
+                <h4 className={styles.toolText}>Stroke Color</h4>
+                <div className={styles.itemContainer}>
+                    <div className={styles.colorBox} style={{backgroundColor: COLORS.BLACK}} />
+                    <div className={styles.colorBox} style={{backgroundColor: COLORS.RED}} />
+                    <div className={styles.colorBox} style={{backgroundColor: COLORS.GREEN}} />
+                    <div className={styles.colorBox} style={{backgroundColor: COLORS.BLUE}} />
+                    <div className={styles.colorBox} style={{backgroundColor: COLORS.ORANGE}} />
+                    <div className={styles.colorBox} style={{backgroundColor: COLORS.YELLOW}} />
+                </div>
+            </div>
+            <div className={styles.toolItem}>
+                <h4>Brush Size</h4>
+                <div>
+                    <input type="range" min={1} max={10} step={1} onChange={handleBrushSize} />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Toolbox;
